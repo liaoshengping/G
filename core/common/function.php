5c960e59b152c671b6ec40de06b8481557b05511
廖 <lsp@nongline.com>
2019-07-22 08:56:29 +0800

    '12'

--- core/common/function.php
+++ core/common/function.php
@@ -11,12 +11,13 @@
  * @param string $theTarget 目标
  * @param array $render_data 渲染数据s
  */
- function generate($origin='' , $theTarget='', $render_data=[],$type=''){
-     //储存在文件夹，则创建
-
-//     if(is_dir()){
-//
-//     }
-    $render = render($origin,$render_data);
-    file_put_contents($theTarget,$render);
+function generate($origin = '', $theTarget = '', $render_data = [], $type = '')
+{
+    //储存在文件夹，则创建
+    $dir = dirname($theTarget);
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777,1);
+    }
+    $render = render($origin, $render_data);
+    file_put_contents($theTarget, $render);
 }
